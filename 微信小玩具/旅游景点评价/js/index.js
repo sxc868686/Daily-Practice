f7a911c828cf49b0447a24575c75ab35df38b970
sxc868686 <eveonile@gmail.com>
2017-02-06 14:39:35 +0800

    准备工作
    准备一点初始代码

---
+++ 微信小玩具/旅游景点评价/js/index.js
@@ -0,0 +1,32 @@
+/**
+ * Created by Administrator on 2017/2/6.
+ */
+function id(obj) {
+    return document.getElementById(obj);
+}
+function bind(obj, ev, fn) {
+    if (obj.addEventListener) {
+        obj.addEventListener(ev, fn, false);
+    } else {
+        obj.attachEvent('on' + ev, function() {
+            fn.call(obj);
+        });
+    }
+}
+function view() {
+    return {
+        w: document.documentElement.clientWidth,
+        h: document.documentElement.clientHeight
+    };
+}
+function addClass(obj, sClass) {
+    var aClass = obj.className.split(' ');
+    if (!obj.className) {
+        obj.className = sClass;
+        return;
+    }
+    for (var i = 0; i < aClass.length; i++) {
+        if (aClass[i] === sClass) return;
+    }
+    obj.className += ' ' + sClass;
+}
