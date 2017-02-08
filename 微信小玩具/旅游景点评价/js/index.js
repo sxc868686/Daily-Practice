8e8711f53e495e5415b99bbdb316edc4e256a66f
sxc868686 <eveonile@gmail.com>
2017-02-08 16:38:54 +0800

    手指滑动完成
    代码完成

--- 微信小玩具/旅游景点评价/js/index.js
+++ 微信小玩具/旅游景点评价/js/index.js
@@ -1,6 +1,3 @@
-/**
- * Created by Administrator on 2017/2/6.
- */
 function id(obj) {
     return document.getElementById(obj);
 }
@@ -54,16 +51,22 @@ window.onload=function () {
         var tab=id('tabPic');
         var tList=id('picList');
         var tNav=tab.getElementsByTagName('nav')[0].children;
-        var tNow=0;//当前选中的项，默认第0个
+        var tNow=0;//当前显示的图片，默认第0个
         var iX=0; //记录translateX需要移动的值
         var iW=view().w; //记录屏幕的宽度
         var oTimer=null;//定时器
+        var iStartTouchX=0;//记录开始时的x轴坐标
+        var iTouchX=0;//记录手指按下的x轴坐标
 
-        oTimer=setInterval(function () {
-            tNow++;
-            tNow=tNow%tNav.length;
-            Tab();
-        },2000);
+        //自动播放tab
+        function auto() {
+            oTimer=setInterval(function () {
+                tNow++;
+                tNow=tNow%tNav.length;
+                Tab();
+            },2000);
+        }
+
         //执行代码
         function Tab() {
             iX=-tNow*iW; //每次移动一个屏幕
@@ -74,6 +77,50 @@ window.onload=function () {
             };
             addClass(tNav[tNow],'active');
         };
+        //因为on不一定支持animation,使用自定义的方法替代
+        bind(tab,'touchstart',fnStart);
+        bind(tab,'touchmove',fnMove);
+        bind(tab,'touchend',fnEnd);
+        auto();
+        //清除默认的touchmove事件
+        bind(document,'touchmove',function (ev) {
+            ev.preventDefault();
+        })
+        //手指滑动开始事件
+        function fnStart(ev) {
+            clearInterval(oTimer);
+            tList.style.WebkitTransition='none';//清除延迟，否则滑动如同生锈一般
+            var ev=ev || window.event;
+            ev=ev.changedTouches[0];
+            //console.log(ev); 得到changedTouches的参数列表
+            iStartTouchX=ev.pageX;
+            iTouchX=iX;
+        };
+        //滑动事件
+        function fnMove(ev) {
+            var ev=ev || window.event;
+
+            ev=ev.changedTouches[0];
+            var iDis=ev.pageX-iStartTouchX;//得到出发点和移动后的终点距离的差值
+            iX=iTouchX+iDis;
+            tList.style.WebkitTransform='translateX('+iX+'px)';
+        };
+        //滑动结束事件
+        function fnEnd() {
+            //得到移动后该显示哪个图片的值
+            tNow=iX/iW;
+            tNow=-Math.round(tNow);
+            if(tNow<0){
+                tNow=0;
+            }else if(tNow>tNav.length-1){
+                tNow=tNav.length-1;
+            }
+            tNow=tNow%tNav.length;
+            Tab();
+            auto();
+        }
+
     };
+
     fnTab();
 };
